9b63d173e162d8bfe6f47d1871b3335c1af40e0b
VETRISELVAN E <140419979+vetri640@users.noreply.github.com>
2026-05-31 13:35:29 +0530

    Add files via upload

---
+++ client/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      }
+    }
+  },
+  build: {
+    // This increases the allowed bundle size warning limit to 1.6MB
+    chunkSizeWarningLimit: 1600, 
+  }
+})
